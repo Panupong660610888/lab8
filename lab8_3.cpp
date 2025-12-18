3b0bb186ac294510dffbbcf0d5661d750e2d11a2
panupong660610888 <panupong_wang@cmu.ac.th>
2025-12-19 00:43:53 +0700

    edited__

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,13 +1,45 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    
+    int age;
+    double height;
+    long long int bounty;
+    string character;
+    
+    cout << "Enter your age: ";
+    cin >> age;
+    
+    if (age <= 25){
+        cout << "Enter your height: ";
+        cin >> height;
+               if (height < 100){
+                  character = "Chopper";
+               }else if (height < 180)
+                  character = "Usopp";
+                     else {
+                       cout << "Enter your bounty: ";
+                       cin >> bounty;
+                          if (bounty > 1100e6)
+                             character = "Zoro";
+                          else 
+                             character = "Sanji";
+                          }
+        
+    }else if (age <= 60){
+        cout << "Enter your bounty: ";
+        cin >> bounty;
+               if (bounty > 500e6){
+                  character = "Jinbe";
+              }else
+                  character = "Franky";
+         }else
+             character = "Brook";
+        
+    cout << "Your character = " << character;
+    
+    return 0;
+}
